d8fb3837b459f5da7f32212da1d31e02211d1d0c
SZUZOUXu <96175085+SZUZOUXu@users.noreply.github.com>
2022-03-04 09:28:33 +0800

    Update 1001 A+B Format.cpp

--- 1001 A+B Format.cpp
+++ 1001 A+B Format.cpp
@@ -1,63 +1,17 @@
 #include<iostream>
 using namespace std;
-/*
-int main()
-{
-	int a, b, len = 0;
-	cin >> a >> b;
-	int c = a + b;
 
-	int k = c;
-	if (k < 0)
-		k = -k;
-	while (k > 0) {
-		k /= 10;
-		len++;
-	}
-	if (c == 0)//¶ÔÓÚÏà¼ÓÎª0µÄÇé¿ö
-		len = 1;
-	int yu, i;
-	yu = len % 3;
-	int t[8];
-	k = c;
-	if (k < 0)
-		k = -k;
-	for (i = 0;i < len;i++) {
-		t[i] = k % 10;
-		k /= 10;
-	}
-	len--;
-	if (c < 0)
-		cout << "-";
-	int flag = 0;//ÅĞ¶ÏÊÇ·ñ´æÔÚÓàÊı
-	while (yu > 0) {
-		cout << t[len];
-		len--;
-		yu--;
-		flag = 1;
-	}
-
-	while (len >= 0) {
-		if ((len + 1) % 3 == 0 && flag == 1)
-			cout << ",";
-		cout << t[len];
-		len--;
-		flag = 1;
-	}
-	return 0;
-}
-*/
 int main()
 {
 	int a, b;
 	cin >> a >> b;
-	char ans[8];//´æ·ÅÊı¾İ×Ö·û´®ÀàĞÍ
+	char ans[8];//å­˜æ”¾æ•°æ®å­—ç¬¦ä¸²ç±»å‹
 	sprintf_s(ans, sizeof(ans), "%d", a + b);
 	int len = strlen(ans);
 	for (int i = 0;i < len;i++) {
 		cout << ans[i];
 		if (ans[i] != '-' && (i != len - 1) && (len - i - 1) % 3 == 0) 
-			cout << ",";//ºóÃæÊ£ÏÂ3n¸öÊıÊ± Òª¼Ó',' 
+			cout << ",";//åé¢å‰©ä¸‹3nä¸ªæ•°æ—¶ è¦åŠ ',' 
 	}
 	return 0;
 }
